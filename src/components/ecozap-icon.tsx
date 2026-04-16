abdf6757dc5467190a7729f10763368c51809542
Jhowsilva2k22 <101504546+Jhowsilva2k22@users.noreply.github.com>
2026-04-16 04:15:43 -0300

    feat: redesign EcoZap icon — chat bubble with leaf (matches new brand logo)

--- src/components/ecozap-icon.tsx
+++ src/components/ecozap-icon.tsx
@@ -11,13 +11,15 @@ export function EcoZapIcon({ className }: EcoZapIconProps) {
       xmlns="http://www.w3.org/2000/svg"
       aria-label="EcoZap"
     >
-      {/* Leaf silhouette */}
-      <path d="M12 2C8.5 2 5 5.5 5 10c0 3 1.5 5.5 3.8 7L9 20h6l.2-3C17.5 15.5 19 13 19 10c0-4.5-3.5-8-7-8z" />
-      {/* Lightning bolt highlight */}
+      {/* Chat bubble outline with tail */}
       <path
-        d="M13.5 8.5l-3.5 5.5h2.8l-1.3 4 4.5-6.5H13l.5-3z"
-        fill="white"
-        opacity="0.35"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6.5 2C4.01 2 2 4.01 2 6.5v7C2 15.99 4.01 18 6.5 18H7v2.88c0 .69.84 1.04 1.33.55L11.67 18H17.5c2.49 0 4.5-2.01 4.5-4.5v-7C22 4.01 19.99 2 17.5 2h-11zM4 6.5C4 5.12 5.12 4 6.5 4h11C18.88 4 20 5.12 20 6.5v7c0 1.38-1.12 2.5-2.5 2.5h-6.33L9 18.13V16H6.5C5.12 16 4 14.88 4 13.5v-7z"
+      />
+      {/* Leaf */}
+      <path
+        d="M12 6.5c-2.8.7-4.8 3.2-4.5 6.5.7-.9 1.8-1.7 3.1-2.1-.2 1.4 0 2.8.9 4 .9-1.2 1.1-2.6.9-4 1.3.4 2.4 1.2 3.1 2.1.3-3.3-1.7-5.8-3.5-6.5z"
       />
     </svg>
   )
